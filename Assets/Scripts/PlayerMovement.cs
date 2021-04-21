1e17ce9ef5845321ae3b35abf9fb423bcf3093d9
Minh <minhanhduong33@gmail.com>
2021-04-21 01:25:15 -0500

    Player can Move and Jump

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,15 +4,27 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public PlayerController2D _controller;
+    public Animator _animator;
+    public float _runSpeed = 40f;
+    float _horizontalMove = 0f;
+    bool _jump = false;
+    bool _crouch = false;
 
     // Update is called once per frame
     void Update()
     {
-        
+        _horizontalMove = Input.GetAxisRaw("Horizontal") * _runSpeed;
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            _jump = true;
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        _controller.Move(_horizontalMove * Time.fixedDeltaTime, _crouch, _jump);
+        _jump = false;
     }
 }
