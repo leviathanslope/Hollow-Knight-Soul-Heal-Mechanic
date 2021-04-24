c950757482017735cd0ab5cdadaa2a2014d33a95
Minh <minhanhduong33@gmail.com>
2021-04-24 13:40:50 -0500

    Animations for Damage/Death. Health Bar UI

---
+++ Assets/Scripts/PlayerCharacter.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Events;
+
+public class PlayerCharacter : MonoBehaviour
+{
+    AudioSource _audioSource = null;
+    public Animator _animator;
+
+    public int maxHealth = 100;
+    public int currentHealth;
+
+    public HealthBar healthBar;
+
+    [SerializeField] AudioClip _damageSFX = null;
+    [SerializeField] AudioClip _deathSFX = null;
+
+    public UnityEvent OnDieEvent;
+    public UnityEvent OnDamageEvent;
+
+    private void Start()
+    {
+        currentHealth = maxHealth;
+        healthBar.SetMaxHealth(maxHealth);
+    }
+
+    void Update()
+    {
+        
+    }
+
+    public void TakeDamage(int damage)
+    {
+        currentHealth -= damage;
+        healthBar.SetHealth(currentHealth);
+        if (_damageSFX != null)
+        {
+            _audioSource.PlayOneShot(_damageSFX, 1f);
+        }
+
+        if (currentHealth <= 0)
+        {
+            Die();
+        } else
+        {
+            OnDamageEvent.Invoke();
+        }
+    }
+
+    public void Die()
+    {
+        Debug.Log("Player has been killed!");
+        DisableOnDeathObjects();
+    }
+
+    private void DisableOnDeathObjects()
+    {
+        OnDieEvent.Invoke();
+    }
+
+    public void OnDie()
+    {
+        _animator.SetBool("IsDead", true);
+    }
+
+    public void OnDamage()
+    {
+        _animator.SetBool("IsHurt", true);
+    }
+}
